debe3a6799401cef42c5f96a2531fa85ece00f28
Szymon Piórkowski <szymmon123@gmail.com>
2017-10-31 00:59:32 +0100

    Added first version of html files.

--- mainGDMS/urls.py
+++ mainGDMS/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^browse', views.browse, name = 'browse' ),
+	url(r'^edit', views.edit, name = 'edit' ),
     url(r'^$', views.index, name = 'indexName' )
 ]
--- mainGDMS/views.py
+++ mainGDMS/views.py
@@ -5,4 +5,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainGDMS/home.html')
+	
+def browse(request):
+    return render(request, 'mainGDMS/browse.html')
+	
+def edit(request):
+    return render(request, 'mainGDMS/edit.html')
 
