69940d9145ec26e52760cdd10285a9f825fa3340
szymon112233 <szymmon123@gmail.com>
2017-11-04 16:17:46 +0100

    Added pycharm proejct files, changed login html file structure

--- mainGDMS/urls.py
+++ mainGDMS/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
+
 
 urlpatterns = [
 	url(r'^browse', views.browse, name = 'browse' ),
 	url(r'^edit', views.edit, name = 'edit' ),
-    url(r'^$', views.index, name = 'indexName' )
+    url(r'^$', login, {'template_name': 'mainGDMS/login.html'} ,name = 'indexName' )
 ]
