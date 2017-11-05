518014be559f5a6147f42db2339b5afe8e5a03fb
Szymon Piórkowski <szymmon123@gmail.com>
2017-11-05 13:09:17 +0100

    Ignored .idea directory, added GameUser table to DB

--- mainGDMS/admin.py
+++ mainGDMS/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from mainGDMS.models import GameUser
 
-# Register your models here.
+admin.site.register(GameUser)
+
--- mainGDMS/models.py
+++ mainGDMS/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class GameUser(models.Model):
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField()
+    date_lastlogin = models.DateTimeField()
+
+    def __str__(self):
+        return self.login
