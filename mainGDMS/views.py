4531d9864cf53998f4cd2e2554ddb7edf735411a
Szymon Piórkowski <szymmon123@gmail.com>
2017-11-11 15:52:21 +0100

    Added login requirements to browse & edit files.

--- mainGDMS/views.py
+++ mainGDMS/views.py
@@ -2,13 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'mainGDMS/home.html')
-	
+
+@login_required(login_url='/')
 def browse(request):
     return render(request, 'mainGDMS/browse.html')
-	
+
+@login_required(login_url='/')
 def edit(request):
     return render(request, 'mainGDMS/edit.html')
 
